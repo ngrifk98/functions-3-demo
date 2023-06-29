2b7733bdc527f01628a6031bbf74f3edd86636dc
Nick Kingston <ngrifk@gmail.com>
2023-06-29 14:39:44 -0500

    functions-3-third-update

--- starter.js
+++ starter.js
@@ -50,7 +50,7 @@ const snakeCaseArray = copyArrToSnakeCase(lotr)
 console.log('Camel Case Array:')
 console.log(camelCaseArray)  // ["biLbOBaGGINs", "leGOlAs", "FrodoBaGGINS", "sAMwiSeGamGEe", "gAndALFtHeGREY"]
 
-console.log('Snake Case Array:')
+console.log('\nSnake Case Array:')
 console.log(snakeCaseArray)  // ["bilbo_baggins", "legolas", "frodo_baggins", "samwise_gamgee", "gandalf_the_grey"]
 
 console.log('\nHigher Order Array Methods:')
@@ -60,13 +60,15 @@ console.log('---------------------------')
 const colors = ['red', 'blue', 'yellow', 'green', 'orange']
 
 console.log('Mapped Colors:')
+
 // Pass a callback to map that will return 'pink' for each color in the array.
 const mappedColors = colors.map(color => 'pink')
 console.log(mappedColors)  // ["pink", "pink", "pink", "pink", "pink"]
 
 const formalNames = ['Bernard', 'Elizabeth', 'Conrad', 'Mary Margaret']
 
-console.log('Formal Greetings:')
+console.log('\nFormal Greetings:')
+
 // Edit the formalGreeting function and use the built-in .map method to map over the names parameter and return a new array with "Hello, " appended to the beginning of each name.
 const formalGreeting = names => {
   return names.map(name => `Hello, ${name}`)
@@ -76,7 +78,7 @@ console.log(formalGreeting(formalNames))  // ["Hello, Bernard", "Hello, Elizabet
 
 const places = ['Binghampton', 'Albany', 'New York', 'Ithaca', 'Auburn', 'Rochester', 'Buffalo']
 
-console.log('Places that Start with A:')
+console.log('\nPlaces that Start with A:')
 // Pass a callback to filter that will return only strings that begin with the letter A.
 const placesThatStartWithA = places.filter(place => place.startsWith('A'))
 console.log(placesThatStartWithA)  // ["Albany", "Auburn"]
@@ -87,7 +89,7 @@ let jobs = [
   { designer: "Alicia" },
 ];
 
-console.log('Identifier - Person with Programmer Job:')
+console.log('\nIdentifier - Person with Programmer Job:')
 // Create a function called identifier that uses the filter higher-order array method to filter over the provided jobs array of objects.
 // The function should return the object of the person with a job as a programmer.
 const identifier = jobs.filter(job => job.programmer)[0]
@@ -95,7 +97,7 @@ console.log(identifier)  // { programmer: "Steve" }
 
 const numsToReduce = [43, 7, 24, 79, 290]
 
-console.log('Product of Array:')
+console.log('\nProduct of Array:')
 // Edit the productOfArray function and use the built-in .reduce method to loop over the numbers parameter and return the product of all the numbers in the array.
 const productOfArray = numbers => {
   return numbers.reduce((acc, curr) => acc * curr)
@@ -111,7 +113,7 @@ const expenses = [
   { title: 'food', amount: 300 }
 ]
 
-console.log('Remaining Budget:')
+console.log('\nRemaining Budget:')
 // Pass a callback and an initial value to reduce that will subtract all the expenses in the array from the initial budget.
 const remaining = expenses.reduce((budget, expense) => budget - expense.amount, budget)
 console.log(remaining)  // 450
